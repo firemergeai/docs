e6b4d65ef4bbf0ef5b3d99e1dbc775bcb87805f2
Tirumal Rao <tirumal.rao.india@gmail.com>
2024-03-22 12:36:41 +0530

    fix: edit link changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/firemergeai/docs/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/firemergeai/docs/blob/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
